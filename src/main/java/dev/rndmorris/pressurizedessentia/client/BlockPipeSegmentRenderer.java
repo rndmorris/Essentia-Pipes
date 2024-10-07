60295b1f2a38f478c878977ecce93f950ae4835a
rndmorris <13648552+rndmorris@users.noreply.github.com>
2024-10-07 14:45:20 -0500

    Reproduce original rendering

--- src/main/java/dev/rndmorris/pressurizedessentia/client/BlockPipeSegmentRenderer.java
+++ src/main/java/dev/rndmorris/pressurizedessentia/client/BlockPipeSegmentRenderer.java
@@ -7,9 +7,10 @@ import cpw.mods.fml.relauncher.SideOnly;
 import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderBlocks;
 import net.minecraft.world.IBlockAccess;
+import thaumcraft.client.renderers.block.BlockRenderer;
 
 @SideOnly(Side.CLIENT)
-public class BlockPipeSegmentRenderer implements ISimpleBlockRenderingHandler {
+public class BlockPipeSegmentRenderer extends RenderBlocks implements ISimpleBlockRenderingHandler {
 
     public static int renderId;
     public static BlockPipeSegmentRenderer instance = new BlockPipeSegmentRenderer();
@@ -22,17 +23,19 @@ public class BlockPipeSegmentRenderer implements ISimpleBlockRenderingHandler {
 
     @Override
     public void renderInventoryBlock(Block block, int metadata, int modelId, RenderBlocks renderer) {
-
+        final var icon = block.getIcon(0, metadata);
+        BlockRenderer.drawFaces(renderer, block, icon, false);
     }
 
     @Override
     public boolean renderWorldBlock(IBlockAccess world, int x, int y, int z, Block block, int modelId, RenderBlocks renderer) {
-        return false;
+        renderer.setRenderBoundsFromBlock(block);
+        return renderer.renderStandardBlock(block, x, y, z);
     }
 
     @Override
     public boolean shouldRender3DInInventory(int modelId) {
-        return false;
+        return true;
     }
 
     @Override
